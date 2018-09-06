79d7533df56a02b6ce2d263587f68c862485a318
Blaithin Settle <blaithin.settle@yahoo.com>
2018-09-06 10:27:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price= Math.floor(Math.random() * (100-1) + 1);
+ var obj={
+   itemName:item,
+   itemPrice:price
+ }
+ 
+ 
+ return `${item} ${price}`
+ 
 }
 
 function viewCart() {
